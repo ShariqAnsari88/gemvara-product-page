9fffbe4e00ed7d370844899bc52e2b5990d38d1f
ShariqAnsari88 <shariqatwork@gmail.com>
2020-07-12 00:33:17 +0530

    create object for slick slider in app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,31 @@
-console.log("its working...");
+
+$(document).ready(function(){
+
+    const slickSlider = {
+        productCarousel: $('.product-slider'),
+        productCarouselNav: $('.slider-nav'),
+
+        slick: function(el, slidesToShow, slidesToScroll, arrows, fade, asNavFor, dots, centerMode, focusOnSelect){
+            
+            el.slick({
+                slidesToShow: slidesToShow,
+                slidesToScroll: slidesToScroll,
+                arrows: arrows,
+                fade: fade,
+                asNavFor: asNavFor,
+                dots: dots,
+                centerMode: centerMode,
+                focusOnSelect: focusOnSelect
+            });
+
+        },
+        init: function(){
+            slickSlider.slick(slickSlider.productCarousel,1,1,false,true,'.slider-nav',false,null,null);
+            slickSlider.slick(slickSlider.productCarouselNav,3,1,false,false,'.product-slider',false,true,true);
+        }
+    }
+
+    //Functions initialization
+    slickSlider.init();
+
+});
